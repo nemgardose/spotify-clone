73a3fc94c545a53e5a34a3670361a421c4c6db18
nemgardose <gardosenemwel@gmail.com>
2020-08-31 21:44:22 +0800

    Finished Layout for Playing Bar Container

--- index.php
+++ index.php
@@ -20,17 +20,35 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+
+    <div class="mainContainer">
+        
+    </div>
+
     <div id="nowPlayingBarContainer">
         <div id="nowPlayingBar">
             <div id="nowPlayingLeft">
+                <div class="content">
+                    <span class="albumLink">
+                        <img src="https://i.scdn.co/image/ab67616d0000b27395e6bb2633a09d9c7c8eaa6d" class="albumArtwork">
+                    </span>
+                    <div class="trackInfo">
+                        <span class="trackName">
+                            <span>Someone That Loves You</span>
+                        </span>
 
+                        <span class="artistName">
+                            <span>HONNE</span>
+                        </span>
+                    </div>
+                </div>
             </div>
 
             <div id="nowPlayingCenter">
                 <div class="content playerControls">
                     <div class="buttons">
                         <button class="controlButton shuffle" title="Shuffle">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle" class="albumArtwork"> 
                         </button>
 
                         <button class="controlButton previous" title="Previous">
@@ -52,15 +70,34 @@
                         <button class="controlButton repeat" title="Repeat">
                             <img src="assets/images/icons/repeat.png" alt="Repeat">
                         </button>
-
                     </div>
+
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
+
                 </div>
             </div>
 
             <div id="nowPlayingRight">
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="Volume Button">
+                        <img src="assets/images/icons/volume.png" alt="Volume">
+                    </button>
 
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     </div>
 </body>
