56cf64ceb3bd3209110cce62862a57e5127b0ce9
nemgardose <gardosenemwel@gmail.com>
2020-09-01 20:40:41 +0800

    Restructured the index files into three contents

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php   
+    include("includes/config.php");
+    //session_destroy(); //for logout
+
+    if(isset($_SESSION['userLoggedIn'])) {
+        $userLoggedIn = $_SESSION['userLoggedIn'];
+    } else {
+        header("Location: register.php");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.ico">
+    <title>Spotify Clone</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="mainContainer">
+        <div id="topContainer">
+            <?php include("includes/navbarContainer.php")?>
+                <div id="mainViewContainer">
+                    <div id="mainContent">
--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<?php   
-    include("includes/config.php");
-    //session_destroy(); //for logout
+<?php include("includes/header.php") ?>
 
-    if(isset($_SESSION['userLoggedIn'])) {
-        $userLoggedIn = $_SESSION['userLoggedIn'];
-    } else {
-        header("Location: register.php");
-    }
+<?php include("includes/footer.php") ?>
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="favicon.ico">
-    <title>Spotify Clone</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <div class="mainContainer">
-
-        <div id="topContainer">
-            <?php include("includes/navbarContainer.php")?>
-
-           <div id="mainViewContainer">
-               <div id="mainContent">
-                   
-               </div>
-           </div>
-
-
-        </div>
-
-        <?php include("includes/nowPlayingBar.php")?>
-
-    </div>
-</body>
-</html>
